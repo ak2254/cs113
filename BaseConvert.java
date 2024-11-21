894675b3477e43eebb05e5cd8343532d1c06c161
ak2254 <51174202+ak2254@users.noreply.github.com>
2024-11-21 10:54:29 -0500

    Update BaseConvert.java

--- BaseConvert.java
+++ BaseConvert.java
@@ -44,4 +44,72 @@ for employee in data.keys():
 for relation in result:
     print(relation)
 
+# Sample dataset as a list of dictionaries
+data = [
+    {"Employee": "Anjali", "Manager": "Nick"},
+    {"Employee": "Nick", "Manager": "Chris"},
+    {"Employee": "Chris", "Manager": None}
+]
+
+# Function to find a manager for a given employee
+def find_manager(employee, data):
+    for record in data:
+        if record["Employee"] == employee:
+            return record["Manager"]
+    return None
+
+# Function to find all managers for an employee
+def get_all_managers(employee, data):
+    managers = []
+    while employee:
+        manager = find_manager(employee, data)
+        if manager:
+            managers.append(manager)
+        employee = manager
+    return managers
+
+# Generate the desired output
+result = []
+for record in data:
+    employee = record["Employee"]
+    all_managers = get_all_managers(employee, data)
+    for manager in all_managers:
+        result.append({"Manager": manager, "Employee": employee})
+
+# Print the results
+for relation in result:
+    print(relation)
+
+
+
+
+# Sample dataset as a list of tuples
+data = [
+    ("Anjali", "Nick"),
+    ("Nick", "Chris"),
+    ("Chris", None)
+]
+
+# Function to find all managers for an employee
+def get_all_managers(employee, data):
+    managers = []
+    while employee:
+        # Find the manager for the current employee
+        manager = next((m for e, m in data if e == employee), None)
+        if manager:
+            managers.append(manager)
+        employee = manager  # Move up the hierarchy
+    return managers
+
+# Generate the desired output
+result = []
+for employee, _ in data:
+    all_managers = get_all_managers(employee, data)
+    for manager in all_managers:
+        result.append((manager, employee))
+
+# Print the results
+for relation in result:
+    print(relation)
+
 
